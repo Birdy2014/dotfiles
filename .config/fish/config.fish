fc22d59963a3d6fab89a05bff16076677ef03aa0
Moritz Vogel <moritzv7@googlemail.com>
2022-07-29 23:48:15 +0200

    update configs for fish, lf, sway, waybar, add scripts
    new scripts:
    - playerctl-current: Wrapper around playerctl that picks the current or
    last playing player
    - start-sway: script that starts sway with the correct environment
    variables set
    
    fish:
    - use the start-sway script on tty1
    - use modified boxfish theme instead of oh-my-fish
    
    lf:
    - fix max image width check
    
    sway:
    - use the playerctl-current script
    - use swaymsg exec to launch applications from rofi
    - set monospace font for gtk
    - change window border/titlebar colors
    
    waybar:
    - use playerctl-current
    - display all icons on the left

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,12 +8,6 @@ if status is-interactive
     set fish_greeting
     fish_vi_key_bindings
 
-    if not test -d "$HOME/.local/share/omf"
-        curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > /tmp/install-omf
-        fish /tmp/install-omf --path=$HOME/.local/share/omf --config=$HOME/.config/omf
-        omf install boxfish
-    end
-
     alias ls 'ls --color=auto'
     alias l 'ls -lAh'
     alias tm 'tmux new-session -A -s main'
@@ -25,26 +19,65 @@ end
 
 if status is-login
     if test (tty) = "/dev/tty1"
-        set -gx XDG_CURRENT_DESKTOP sway
-        set -gx XDG_SESSION_DESKTOP sway
-        set -gx XDG_SESSION_TYPE wayland
-
-        # enable wayland in applications
-        set -gx MOZ_ENABLE_WAYLAND 1
-
-        # theming
-        set -gx XCURSOR_THEME LyraX
-        set -gx GTK_THEME Gruvbox-Material-Dark
-        set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
-        set -gx QT_QPA_PLATFORMTHEME qt5ct
-
-        # workarounds
-        set -gx _JAVA_AWT_WM_NONREPARENTING 1
-        set -gx WLR_NO_HARDWARE_CURSORS 1 # Workaround for invisible cursor on nvidia
-
-        exec sway --unsupported-gpu
+        exec start-sway
     else if test (tty) = "/dev/tty2"
         exec startx
+    end
+end
+
+# Based on https://github.com/joelwanner/theme-boxfish
+function fish_prompt
+    set -l last_status $status
+    set -l cwd (prompt_pwd)
+
+    # Display hostname if in ssh session
+    if test -n "$SSH_CLIENT"; or test -n "$SSH_TTY"
+        set_color black -b yellow
+        echo -n " $(hostname -s) "
+    end
+
+    # Show if last command was not successful
+    if not test $last_status -eq 0
+        set_color --bold white -b red
+        echo -n ' ! '
+        set_color normal
+    end
+
+    # Display current path
+    set_color black -b blue
+    echo -n " $cwd "
+
+    # Show git branch and dirty state
+    set -l git_branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    set -l git_dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
+    if test -n "$git_branch"
+        if test -n "$git_dirty"
+            set_color black -b yellow
+            echo -n " $git_branch "
+        else
+            set_color black -b green
+            echo -n " $git_branch "
+        end
+    end
+
+    # Add a space
+    set_color normal
+    echo -n ' '
+end
+
+function fish_mode_prompt
+    if [ $fish_key_bindings = fish_vi_key_bindings ]
+        switch $fish_bind_mode
+            case default
+                set_color -b red
+            case insert
+                set_color -b green
+            case visual
+                set_color -b yellow
+            case replace-one
+                set_color -b magenta
+        end
+        echo -n " "
     end
 end
 
--- .config/waybar/scripts/playerctl.sh
+++ .config/waybar/scripts/playerctl.sh
@@ -2,13 +2,13 @@
 
 command -v playerctl &> /dev/null || exit 1
 
-status="$(playerctl status 2>&1)"
+status="$(playerctl-current status 2>&1)"
 case "$status" in
     Paused) status_sym='';;
     Playing) status_sym='';;
     *) exit 0;;
 esac
 
-full_title="$(playerctl metadata | grep xesam:title | sed -e 's/^.*title\s*//' | sed 's/&/&amp;/g')"
+full_title="$(playerctl-current metadata xesam:title | sed 's/&/&amp;/g')"
 [ ${#full_title} -gt 28 ] && short_title="$(cut -c 1-25 <<< $full_title)..." || short_title="$full_title"
 echo -n -e "$status_sym $short_title\n$full_title"
